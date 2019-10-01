ea088ccedc407ae5eba279285b4e2c001e2ec6e3
ludsan0507 <ludsan0507@gmail.com>
2019-10-01 15:48:26 +0200

    Läxa 2 och 3

---
+++ src/eyyyooo/v40.java
@@ -0,0 +1,26 @@
+package eyyyooo;
+
+import java.util.Scanner;
+
+public class v40 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner input = new Scanner(System.in);
+		
+		int r1 =input.nextInt();
+		int r2 = 0;
+		int s = input.nextInt();
+	
+		r2 = s*2-r1;
+		System.out.println(r2);
+	
+	
+	
+	
+	
+	
+	}
+
+}
