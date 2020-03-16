4a3359262ab8911b87b5e8ff19d7bf1ffa407a53
ludsan0507 <ludsan0507@gmail.com>
2020-03-16 14:50:47 +0100

    Update talspelet.java

--- src/eyyyooo/talspelet.java
+++ src/eyyyooo/talspelet.java
@@ -1,41 +1,66 @@
 package eyyyooo;
 
 import java.util.Scanner;
+
 public class talspelet {
 
 	public static void main(String[] args) {
-		
-	Scanner input = new Scanner(System.in);
-	
-	String guess;
-	int guessIntedger;
-	int tries;
-	int randomint;
-	System.out.println(" VÃ¤lkommen till mitt talspel");
-	System.out.println("");
-	System.out.println(" VÃ¤lj svÃ¥righets grad, LÃ¤tt eller svÃ¥rt?");
-	System.out.println("");
 
-	
+		Scanner input = new Scanner(System.in);
 
+		boolean played = true;
+		while(played) {
+			int randomint;
+			System.out.println(" Välkommen till mitt talspel");
+			System.out.println("");
+			System.out.println(" Välj svårighets grad, Lätt eller svårt?");
+			System.out.println("");
 	
-	 int easyhard = input.nextInt();
-	 boolean played = true;
-	 switch (easyhard) {
-	 
-	 case 1: 
-			System.out.println(" Du har 10 fÃ¶rsÃ¶k pÃ¥ dig!");
-			break;
-	 case 2:
-			System.out.println("Du har 5 fÃ¶sÃ¶k pÃ¥ dig!");
-			break;
-	 }
-		System.out.println(" Grattis du har vunnit!!!!!!!!!");
-		System.out.println(" Du har gissat fel, FÃ¶rsÃ¶k igen!");
+			int easyhard = input.nextInt();
+			switch (easyhard) {
+	
+			case 1:
+				System.out.println(" Du har 10 försök på dig! Gissa ett tal mellan 1-25");
+				randomint = 0 + (int) (Math.random() * ((25 - 0) + 1));
+			
+				for (int tries = 0; tries < 10; tries++) {
+					int gissning = input.nextInt();
+					if (gissning == randomint) {
+						System.out.println(" Grattis du har vunnit!!!!!!!!!");
+						break;
+					} 
+					else if (gissning > randomint)
+						System.out.println(" Du har gissat för högt, Försök igen!");
+					else if (gissning < randomint)
+						System.out.println(" Du har gissat för lågt, Försök igen!");
+	
+				}
+				break;
+			case 2:
+				System.out.println("Du har 5 fösök på dig! Gissa ett tal mellan 1-50");
+				randomint = 0 + (int) (Math.random() * ((50 - 0) + 1));
+				
+				for (int tries = 0; tries < 5; tries++) {
+					int gissning = input.nextInt();
+					if (gissning == randomint) {
+						System.out.println(" Grattis du har vunnit!!!!!!!!!");
+						break;
+					} 
+					else if (gissning > randomint)
+						System.out.println(" Du har gissat för högt, Försök igen!");
+					else if (gissning < randomint)
+						System.out.println(" Du har gissat för lågt, Försök igen!");
+						
+				}
+
+				break;
+				
+			}
+			System.out.println(" Vill du spela igen? 1 för ja 2 för nej");
+			int playagain = input.nextInt();
+			if (playagain == 2) {
+				played = false;
+			}
+		}
 	}
-	
-} 
-	
-	
-
-
+}
